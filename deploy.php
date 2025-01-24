1ec703b4e1ed085b73dc00bf28fdc6bbd77d0387
Mark Gregory <162308433+mark-mpba@users.noreply.github.com>
2025-01-24 15:38:21 +0000

    new host definition

--- deploy.php
+++ deploy.php
@@ -6,7 +6,7 @@ require 'recipe/laravel.php';
 
 // Config
 set('application', 'Laravel Helpdesk Application');          // The Application Title
-set('repository', 'git@github.com:MPBA-Ltd/helpdesk.git');   // SCM Target
+set('repository', 'git@github.com:mark-mpba/helpdesk.git');   // SCM Target
 set('keep_releases', 3);                                     // Number of releases to keep on hosts
 set('default_timeout', 1200);                                // default ssh timeout
 
@@ -20,7 +20,7 @@ add('writable_dirs', ['storage', 'vendor', 'node_modules','bootstrap/cache']);  
 // Hosts
 
 host('prod')
-    ->set('hostname', 'www.mpba.dev')
+    ->set('hostname', 'helpdesk.mpba.dev')
     ->set('remote_user', 'mag')
     ->set('identityFile', '~/.ssh/id_rsa')
     ->set('deploy_path', '/var/www/helpdesk/prod')
@@ -33,7 +33,7 @@ host('prod')
     ->set('ssh_type', 'native');
 
 host('stage')
-    ->set('hostname', 'www.mpba.dev')
+    ->set('hostname', 'helpdesk.mpba.dev')
     ->set('remote_user', 'mag')
     ->set('identityFile', '~/.ssh/id_rsa')
     ->set('deploy_path', '/var/www/helpdesk/stage')
@@ -41,6 +41,19 @@ host('stage')
     ->set('use_relative_symlink', true)
     ->set('http_user', 'mag')
     ->set('branch', 'stage')
+    ->set('ssh_multiplexing', true)
+    ->set('git_tty', false)
+    ->set('ssh_type', 'native');
+
+host('develop')
+    ->set('hostname', 'helpdesk.mpba.dev')
+    ->set('remote_user', 'mag')
+    ->set('identityFile', '~/.ssh/id_rsa')
+    ->set('deploy_path', '/var/www/helpdesk/dev')
+    ->set('writable_use_sudo', false)
+    ->set('use_relative_symlink', true)
+    ->set('http_user', 'mag')
+    ->set('branch', 'develop')
     ->set('ssh_multiplexing', true)
     ->set('git_tty', false)
     ->set('ssh_type', 'native');
