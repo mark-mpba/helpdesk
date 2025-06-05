d07e591720742a3118cd372fdbc878341f3ed0dd
Mark Gregory <mark.gregory@mpba.co.uk>
2025-06-05 10:03:32 +0100

    fixed issue with delete nwo added redis

--- deploy.php
+++ deploy.php
@@ -11,8 +11,8 @@ set('keep_releases', 3);                                     // Number of releas
 set('default_timeout', 1200);                                // default ssh timeout
 
 add('shared_files', ['.env']);                                                    // shared files
-add('shared_dirs', ['storage', 'vendor', 'node_modules','bootstrap/cache']);      // Shared dirs between deploys
-add('writable_dirs', ['storage', 'vendor', 'node_modules','bootstrap/cache']);    // Writable dirs by web server
+add('shared_dirs', ['storage', 'vendor', 'node_modules', 'bootstrap/cache']);      // Shared dirs between deploys
+add('writable_dirs', ['storage', 'vendor', 'node_modules', 'bootstrap/cache']);    // Writable dirs by web server
 
 set('bin/php', function () {
     return '/usr/bin/php8.2';
@@ -24,7 +24,7 @@ set('bin/php', function () {
 // Hosts
 
 host('prod')
-    ->set('hostname', 'helpdesk.mpba.dev')
+    ->set('hostname', 'www.absdev.net')
     ->set('remote_user', 'mag')
     ->set('identityFile', '~/.ssh/id_rsa')
     ->set('deploy_path', '/var/www/helpdesk/prod')
@@ -36,35 +36,9 @@ host('prod')
     ->set('git_tty', false)
     ->set('ssh_type', 'native');
 
-host('stage')
-    ->set('hostname', 'helpdesk.mpba.dev')
-    ->set('remote_user', 'mag')
-    ->set('identityFile', '~/.ssh/id_rsa')
-    ->set('deploy_path', '/var/www/helpdesk/stage')
-    ->set('writable_use_sudo', false)
-    ->set('use_relative_symlink', true)
-    ->set('http_user', 'mag')
-    ->set('branch', 'stage')
-    ->set('ssh_multiplexing', true)
-    ->set('git_tty', false)
-    ->set('ssh_type', 'native');
-
-host('develop')
-    ->set('hostname', 'helpdesk.mpba.dev')
-    ->set('remote_user', 'mag')
-    ->set('identityFile', '~/.ssh/id_rsa')
-    ->set('deploy_path', '/var/www/helpdesk/dev')
-    ->set('writable_use_sudo', false)
-    ->set('use_relative_symlink', true)
-    ->set('http_user', 'mag')
-    ->set('branch', 'develop')
-    ->set('ssh_multiplexing', true)
-    ->set('git_tty', false)
-    ->set('ssh_type', 'native');
-
 
 // Hooks
-after('deploy:success','artisan:config:clear');
-after('deploy:success','artisan:route:clear');
-after('deploy:success','artisan:cache:clear');
+after('deploy:success', 'artisan:config:clear');
+after('deploy:success', 'artisan:route:clear');
+after('deploy:success', 'artisan:cache:clear');
 after('deploy:failed', 'deploy:unlock');
