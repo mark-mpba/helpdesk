d2d3583a584b9400c3130ad44a55e50dc62e928d
Mark Gregory <mark.gregory@mpba.co.uk>
2025-06-05 10:08:10 +0100

    fixed disable register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -60,6 +60,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        return null;
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
