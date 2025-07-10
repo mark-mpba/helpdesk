3251e4b8758d93be8c9ad51f01e27b520490917f
Mark Gregory <mark.gregory@mpba.co.uk>
2025-07-10 18:05:48 +0100

    remove mandatory flag

--- Packages/mpba/laravel-tickets/src/Views/bootstrap3/tests/partials/datatable.blade.php
+++ Packages/mpba/laravel-tickets/src/Views/bootstrap3/tests/partials/datatable.blade.php
@@ -1,16 +1,16 @@
-<table class="ticket-table table table-striped table-bordered dt-responsive nowrap" style="width:100%">
+<table class="ticket-table table table-striped table-bordered" style="width:100%">
     <thead>
-        <tr>
-            <td>{{ trans('ticket::lang.table-id') }}</td>
-            <td>{{ trans('ticket::lang.table-subject') }}</td>
-            <td>{{ trans('ticket::lang.table-status') }}</td>
-            <td>{{ trans('ticket::lang.table-last-updated') }}</td>
-            <td>{{ trans('ticket::lang.table-agent') }}</td>
-          @if( $u->isAgent() || $u->isAdmin() )
+    <tr>
+        <td>{{ trans('ticket::lang.table-id') }}</td>
+        <td>{{ trans('ticket::lang.table-subject') }}</td>
+        <td>{{ trans('ticket::lang.table-status') }}</td>
+        <td>{{ trans('ticket::lang.table-last-updated') }}</td>
+        <td>{{ trans('ticket::lang.table-agent') }}</td>
+        @if( $u->isAgent() || $u->isAdmin() )
             <td>{{ trans('ticket::lang.table-priority') }}</td>
             <td>{{ trans('ticket::lang.table-owner') }}</td>
             <td>{{ trans('ticket::lang.table-category') }}</td>
-          @endif
-        </tr>
+        @endif
+    </tr>
     </thead>
 </table>
