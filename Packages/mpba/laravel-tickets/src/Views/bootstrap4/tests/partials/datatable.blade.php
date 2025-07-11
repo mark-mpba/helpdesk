cc8949665b80f51a22e8838ac4cdcb9061b3b1a0
Mark Gregory <mark.gregory@mpba.co.uk>
2025-07-11 09:22:09 +0100

    layout changes

---
+++ Packages/mpba/laravel-tickets/src/Views/bootstrap4/tests/partials/datatable.blade.php
@@ -0,0 +1,12 @@
+<div class="ml-1 mr-1">
+    <table class="ticket-table table table-striped dt-responsive nowrap" style="width:100%">
+        <thead>
+        <tr>
+            <td>Step</td>
+            <td>Name</td>
+            <td>{{ trans('ticket::lang.table-status') }}</td>
+            <td>{{ trans('ticket::lang.table-last-updated') }}</td>
+        </tr>
+        </thead>
+    </table>
+</div>
